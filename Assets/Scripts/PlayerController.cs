ae5a43685301c19b90d375950cdcd05d0a769133
sofiaana23 <anasofia.eyre@gmail.com>
2024-04-11 00:03:42 -0600

    player movement left and right

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float speed;
+    public float xRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set horizontalinput to receive values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //Left side wall
+        if (transform.position.x < -xRange) 
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        // Right side wall
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
