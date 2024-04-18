45c5ffd53f104de3455bea457ac630485eebf7ed
sofiaana23 <anasofia.eyre@gmail.com>
2024-04-18 15:49:19 -0600

    Collider

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -8,22 +8,25 @@ public class EnemySpawnManager : MonoBehaviour
     public GameObject[] ufoPrefabs; // Array to store UFO ships
     private float spawnRangeX = 150f;
     private float spawnPosZ = 20f;
-    
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPOs = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
-            int ufoIndex = Random.Range(0, ufoPrefabs.Length); //Picks a random UFO from the array
-            Instantiate(ufoPrefabs[ufoIndex],spawnPOs, ufoPrefabs[ufoIndex].transform.rotation); //Spawns index UFO from the array
-        }
+        
+    }
+
+    void SpawnRandomUFO() 
+    {
+        Vector3 spawnPOs = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int ufoIndex = Random.Range(0, ufoPrefabs.Length); //Picks a random UFO from the array
+        Instantiate(ufoPrefabs[ufoIndex], spawnPOs, ufoPrefabs[ufoIndex].transform.rotation); //Spawns index UFO from the array
     }
 }
